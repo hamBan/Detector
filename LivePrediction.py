5ed616142df217d2b121b7bfa7676d8b6200d16f
hamBan <45568675+hamBan@users.noreply.github.com>
2019-11-10 03:00:29 +0530

    Added Timestamp

--- LivePrediction.py
+++ LivePrediction.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import warnings
 warnings.simplefilter(action='ignore')
 import cv2
@@ -5,19 +6,24 @@ from PIL import Image
 import urllib.request
 from firebase import firebase
 from image_module import image_prediction as imp
-firebase = firebase.FirebaseApplication('https://ultra-vision-38b0a.firebaseio.com/', None)
-out = cv2.VideoWriter("FuzzyDetector/output2.avi",cv2.VideoWriter_fourcc(*"MJPG"), 10,(640,480))
+
+#firebase = firebase.FirebaseApplication('https://ultra-vision-38b0a.firebaseio.com/', None)
+firebase = firebase.FirebaseApplication('https://supravision-detector.firebaseio.com/', None)
+out = cv2.VideoWriter("output2.avi",cv2.VideoWriter_fourcc(*"MJPG"), 10,(640,480))
 
 n = 0
 print("Begining Image Feed: ")
 while n<50:
-    image = Image.open(urllib.request.urlopen('http://192.168.43.184:8080/shot.jpg'))
+    image = Image.open(urllib.request.urlopen('http://192.168.1.4:8080/shot.jpg'))
     image = image.resize((640, 480), Image.ANTIALIAS)
     l = imp.prediction(image)
     image_np = l[1]
     fuzzy = l[0]
-    data =  {'FuzzyValue': fuzzy}
-    result = firebase.post('/ultra-vision-38b0a/Customer/',data)
+    now = datetime. now()
+    timestamp = datetime. timestamp(now)
+    data =  {'FuzzyValue': fuzzy,'TimeStamp': timestamp}
+    #result = firebase.post('/ultra-vision-38b0a/Customer/',data)
+    result = firebase.post('supravision-detector/damageReport/',data)
     out.write(image_np)
     n+=1
     print("Frame ",n," Processed")
